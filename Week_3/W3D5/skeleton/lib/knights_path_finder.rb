dc5903b0ef9ef2735649fe8d06212fc6f81eb32c
unknown <kennyla95@gmail.com>
2021-07-26 15:16:03 -0700

    Added build move tree method

--- Week_3/W3D5/skeleton/lib/knights_path_finder.rb
+++ Week_3/W3D5/skeleton/lib/knights_path_finder.rb
@@ -1,44 +1,72 @@
 require_relative "00_tree_node.rb"
+require 'set'
 
 class KnightPathFinder
 
-@@board = Array.new(8){Array.new(8, 0)}
+    @@board = Array.new(8){Array.new(8, 0)}
+    attr_reader :root_node
 
-def initialize(pos)
-    @root_node = PolyTreeNode.new(pos)
-    @considered_positions = [pos]
-    build_move_tree(@root_node) 
+    def initialize(pos)
+        @root_node = PolyTreeNode.new(pos)
+        @considered_positions = Set.new([pos])
+        build_move_tree(@root_node) 
+    end
 
-end
+    def build_move_tree(start_node)
+        q = [] 
 
-def build_move_tree(start_node)
-    return nil
-end 
+        new_move_positions(start_node.value).each do |i| 
+            child_node = PolyTreeNode.new(i)
+            start_node.children << child_node
+            q << child_node
+        end
 
-def self.valid_moves(pos)
+        until q.empty? 
+            curr_node = q.shift 
+            new_move_positions(curr_node.value).each do |i|
+                child = PolyTreeNode.new(i)
+                curr_node.children << child 
+                q << child 
+            end
+        end
 
- row = pos[0]
- col = pos[1]
-possible_move = [
-    [row+2, col+1],
-    [row+2, col-1],
-    [row+1, col+2],
-    [row-1, col+2],
-    [row-2, col+1],
-    [row-2, col-1],
-    [row+1, col-2],
-    [row-1, col-2]  
-]
-result_arr = []
-possible_move.each do |p|
- if p[0] >= 0 && p[1] < 8 && p[0] < 8 && p[1] >= 0
-    result_arr << p 
- end 
-end
-return result_arr
-end
+        start_node
+    end 
+
+    def self.valid_moves(pos)
+        row = pos[0]
+        col = pos[1]
+        possible_move = [
+            [row+2, col+1],
+            [row+2, col-1],
+            [row+1, col+2],
+            [row-1, col+2],
+            [row-2, col+1],
+            [row-2, col-1],
+            [row+1, col-2],
+            [row-1, col-2]  
+        ]
+        result_arr = []
+        possible_move.each do |p|
+            if p[0] >= 0 && p[1] < 8 && p[0] < 8 && p[1] >= 0
+                result_arr << p 
+            end 
+        end
+        return result_arr
+    end
+
+    def new_move_positions(pos)
+        new_moves = KnightPathFinder.valid_moves(pos) 
+
+        res = new_moves.select { |i| !@considered_positions.include?(i)}
+        @considered_positions += res 
+        res 
+    end
 
 end
 
 k = KnightPathFinder.new([0,0])
-p KnightPathFinder.valid_moves([0, 0])
+# p KnightPathFinder.valid_moves([0, 0])
+# p k.new_move_positions([0, 0])
+
+p k.build_move_tree(k.root_node)
