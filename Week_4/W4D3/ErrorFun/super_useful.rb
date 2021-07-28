d08bc81f8932fe426367b8044a9c1fad0e81b924
unknown <kennyla95@gmail.com>
2021-07-28 11:27:06 -0700

    Finished error fun time

--- Week_4/W4D3/ErrorFun/super_useful.rb
+++ Week_4/W4D3/ErrorFun/super_useful.rb
@@ -1,3 +1,7 @@
+class CoffeeError < StandardError
+
+end 
+
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
@@ -10,24 +14,47 @@ def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
   else 
-    raise StandardError 
+    if maybe_fruit == 'coffee' 
+      raise CoffeeError.new("Try again, thanks for coffee") 
+    else 
+      raise ArgumentError.new("No")
+    end
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
-
   puts "Feed me a fruit! (Enter the name of a fruit:)"
+  
+  begin 
   maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e 
+    puts e.message
+    retry
+  rescue ArgumentError => e
+    puts e.message
+  end
+
 end  
+class FriendError < StandardError 
+end 
+
+class ErrorName < StandardError
+end 
+
+class FavPastTimeError < StandardError
+end
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
     @name = name
+    raise ErrorName.new("Name?") if @name.length == 0 
     @yrs_known = yrs_known
+    raise FriendError.new("Less than 5 years?") if @yrs_known < 5
     @fav_pastime = fav_pastime
+    raise FavPastTimeError.new("Past time?") if @fav_pastime.length == 0
   end
 
   def talk_about_friendship
