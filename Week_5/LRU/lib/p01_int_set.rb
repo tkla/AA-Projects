f4f7caf9f95b037f012ee51dae3c829f557b744d
David Oh <davidohyoung@gmail.com>
2021-08-02 16:46:40 -0500

    finished p01

--- Week_5/LRU/lib/p01_int_set.rb
+++ Week_5/LRU/lib/p01_int_set.rb
@@ -32,21 +32,32 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    return false if self.include?(num)
+    self[num] << num
+    return true
   end
 
   def remove(num)
+    self[num].each_with_index do |n, i|
+        if n == num
+          self[num].delete_at(i)
+        end
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -63,18 +74,33 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @store.length <= @count
+    if !self.include?(num)
+      self[num] << num 
+      @count += 1
+      return true
+    end
+    false
   end
 
   def remove(num)
+    self[num].each_with_index do |n, i|
+      if n == num
+        self[num].delete_at(i) 
+        @count -= 1
+      end
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @store.length]
   end
 
   def num_buckets
@@ -82,5 +108,12 @@ class ResizingIntSet
   end
 
   def resize!
+    res = Array.new(@store.length * 2) { Array.new }
+    @store.each do |sub|
+      sub.each do |i|
+        res[i % res.length] << i
+      end
+    end
+    @store = res
   end
 end
