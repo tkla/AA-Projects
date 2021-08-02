72eeddf9edc91ee8b2997563d7c8e561566e8c88
unknown <kennyla95@gmail.com>
2021-08-02 15:40:57 -0700

    Added hash functions for array, string, and hash

--- Week_5/LRU/lib/p02_hashing.rb
+++ Week_5/LRU/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    arr_hash = 0 
+    self.each_with_index do |elem, i| 
+      arr_hash ^= elem.to_i.hash + i 
+    end
+
+    arr_hash 
   end
 end
 
 class String
   def hash
+    str_hash = 0 
+    res = self.split("")
+
+    res.each_with_index do |char, i| 
+      str_hash ^= char.ord.hash + i 
+    end
+    str_hash 
   end
 end
 
@@ -16,6 +29,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_hash = 0 
+    self.keys.each do |k| 
+      hash_hash ^= k.hash + self[k].hash
+    end 
+    hash_hash 
   end
 end
