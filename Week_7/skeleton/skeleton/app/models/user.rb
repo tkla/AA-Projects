044a861a616387a3214d519e689ab212a11067ea
Mauricio <mjlomeli@uci.edu>
2021-08-16 15:01:19 -0700

    changing cats

--- Week_7/skeleton/skeleton/app/models/user.rb
+++ Week_7/skeleton/skeleton/app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
-        if user AND BCrypt::Password.new(user.password_digest).is_password?(password)
+        if user AND is_password?(password)
             user 
         else 
             nil 
@@ -35,4 +35,9 @@ class User < ApplicationRecord
         @password = password 
         self.password_digest = BCrypt::Password.create(password)
     end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
 end
