c3fa5b4ff0129d975830c3daf3a45903d6ea0cdc
your name here <your_email_hereroelliga@yahoo.com>
2021-08-18 13:39:59 -0700

    methods

--- Week_7/w7d3/MyGoals/spec/models/user_spec.rb
+++ Week_7/w7d3/MyGoals/spec/models/user_spec.rb
@@ -14,6 +14,28 @@ RSpec.describe User, type: :model do
 
   #Associations -Todo
 
-  #Controller tests 
+  describe 'find_by_credentials' do 
+    it 'should return a user if given the correct credentials' do 
+      user = User.find_by_credentials('Dog', 'password')
+      expect(user.name).to eq('Dog')
+    end
+
+    it 'should return nil if credentials are incorrect' do 
+      user = User.find_by_credentials('', '')
+      expect(user).to eq(nil)
+    end
+  end
+
+  describe 'password_encryption' do 
+    it 'should not save password to db' do 
+      user = User.find_by(username: 'Dog')
+      expect(user.password).to eq('password')
+      expect(user.password_digest).not_to eq('password')
+    end
+    it 'should encrypt password with bcrypt'
+      expect(BCrypt::Password).to recieve(:create).with('password')
+      FactoryBot.build(:user, password: 'password')
+  end
+end
   
 end
