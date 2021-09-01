3723cc50eed23704bdf4c7353334a95c97608d2c
atjohnfeng <atjohnfeng@gmail.com>
2021-09-01 15:44:39 -0700

    Added additional follow_toggle methods

--- Week_9/w9d3/twitter/app/assets/javascripts/bundle.js
+++ Week_9/w9d3/twitter/app/assets/javascripts/bundle.js
@@ -11,6 +11,48 @@ class FollowToggle {
     constructor(el) {
         this.userId = el.getAttribute("data-user-id");
         this.followState = el.getAttribute("data-initial-follow-state");
+        this.el = el;
+        this.render();
+        this.handleClick();
+    }
+
+    render() {
+        let text = "";
+        if (this.el.getAttribute)
+        if (this.followState === "followed") {
+            text = "Unfollow!";
+        } else {
+            text = "Follow!";
+        }
+        this.el.append(text);
+    }
+
+    handleClick() {
+        let that = this;
+        this.el.addEventListener("click", function(event) {
+            event.preventDefault();
+            $.ajax({
+                url: `/users/${that.userId}/follow`,
+                method: 'post',
+                success: function() {
+                    if (that.followState === "followed") {
+                        that.followState = "unfollowed";
+                    } else {
+                        that.followState = "followed";
+                    };
+                    that.render();
+                },
+                error: function () {
+                    if (that.followState === "followed") {
+                        that.followState = "unfollowed";
+                    } else {
+                        that.followState = "followed";
+                    };
+                    that.render();
+                },
+                dataType: 'JSON',
+            })
+        })
     }
 }
 
@@ -54,7 +96,7 @@ var __webpack_exports__ = {};
 const FollowToggle = __webpack_require__(/*! ./follow_toggle */ "./frontend/follow_toggle.js");
 
 document.addEventListener("DOMContentLoaded", function() {
-    $(".follow-toggle").each( function(i, el){
+    $(".follow-toggle").each(function(i, el){
         const toggle = new FollowToggle(el);
         console.log("Hey");
     })
--- Week_9/w9d3/twitter/frontend/twitter.js
+++ Week_9/w9d3/twitter/frontend/twitter.js
@@ -1,7 +1,7 @@
 const FollowToggle = require("./follow_toggle");
 
 document.addEventListener("DOMContentLoaded", function() {
-    $(".follow-toggle").each( function(i, el){
+    $(".follow-toggle").each(function(i, el){
         const toggle = new FollowToggle(el);
         console.log("Hey");
     })
