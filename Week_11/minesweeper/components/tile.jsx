f3bcfed7c0061a2be18766a18072cb61b9edf920
tkla <kennyla95@gmail.com>
2021-09-13 14:05:34 -0700

    Added handlers for tiles

--- Week_11/minesweeper/components/tile.jsx
+++ Week_11/minesweeper/components/tile.jsx
@@ -9,7 +9,12 @@ export default class Tile extends React.Component {
             explored: this.props.tile.explored,
             flagged: this.props.tile.flagged
         }
+
+        this.alt = false; 
         this.getState = this.getState.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleKeyUp = this.handleKeyUp.bind(this);
     }
 
     getState(){
@@ -20,10 +25,28 @@ export default class Tile extends React.Component {
         } else if (this.state.flagged) {
             return 'f';
         }
-        return '_';
+        return ' ';
+    }
+
+    handleClick(e){
+        this.props.updateGame(this, this.alt);
+    }
+
+    handleKeyDown(e){
+        if(e.key === 'altKey'){
+            this.alt = true;
+        }
+    }
+
+    handleKeyUp(){
+        this.alt = false;
     }
 
     render(){
-        return (<div className='tile'>{this.getState()}</div>);
+        return (<div 
+            onClick={this.handleClick} 
+            onKeyDown={this.handleKeyDown}
+            onlyKeyUp={this.handleKeyUp}
+            className='tile'>{this.getState()}</div>);
     }
 } 
