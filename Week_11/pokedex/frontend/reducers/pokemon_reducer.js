5ce03d7752283efbcf0f3294692284f798cd8c8e
Alex Crooks <alexcrooks2@gmail.com>
2021-09-17 15:47:17 -0700

    Pokemon Reducer started

---
+++ Week_11/pokedex/frontend/reducers/pokemon_reducer.js
@@ -0,0 +1,12 @@
+import { RECEIVE_ALL_POKEMON } from '../actions/pokemon_actions';
+
+export default pokemonReducer = (state = {}, action) => {
+    Object.freeze(state)
+    const nextState = Object.assign({}, state)
+    switch(action.type) {
+        case RECEIVE_ALL_POKEMON: 
+            return {...state, ...action.pokemon}
+        default:
+            return state
+    }
+}
