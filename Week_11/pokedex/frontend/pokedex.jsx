22924dd011a846d65a821fe864bd46aaf8ae0867
Alex Crooks <alexcrooks2@gmail.com>
2021-09-20 13:44:35 -0700

    Skeleton for Pokedex pt 2, w12d1

--- Week_11/pokedex/frontend/pokedex.jsx
+++ Week_11/pokedex/frontend/pokedex.jsx
@@ -5,6 +5,8 @@ import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions'
 import configureStore from './store/store.js'
 import selectAllPokemon from './reducers/selectors'
 import Root from './components/root'
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const store = configureStore();
     const rootEl = document.getElementById('root');
